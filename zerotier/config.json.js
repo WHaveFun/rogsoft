c7d8df86a7254620566256dd7bc18a97e41f33ca
sadog@ubuntu2004 <sadoneli@gmail.com>
2021-04-01 17:48:28 +0800

    fix start up

--- zerotier/config.json.js
+++ zerotier/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"1.3.3",
-"md5":"b440e27dcdce40506068b1cdcea83470",
+"version":"1.3.4",
+"md5":"e7c4a3a07b0475ce45a120d0a9fb1d47",
 "home_url":"Module_zerotier.asp",
 "title":"ZeroTier",
 "description":"ZeroTier 内网穿透",
 "changelog":"",
-"build_date":"2021-04-01_17:23:56"
+"build_date":"2021-04-01_17:48:24"
 }
